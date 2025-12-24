48c00a64a7be419600436aa81a65460efa7a986a
Al3cLee <al3c.wentao.lee@gmail.com>
2025-12-24 11:47:01 +0800

    re-enable lualine with additional config

--- lua/plugins/ef-themes.lua
+++ lua/plugins/ef-themes.lua
@@ -2,6 +2,7 @@ return {
   "oonamo/ef-themes.nvim",
   config = function()
     require("ef-themes").setup({
+      -- See https://github.com/oonamo/ef-themes.nvim/tree/main?tab=readme-ov-file#configuration for the config below
       on_highlights = function(highlights, colors, name)
         return {
           lualine_c_normal = { bg = "NONE" },
